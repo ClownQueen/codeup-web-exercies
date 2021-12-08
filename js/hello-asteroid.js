6062f35ea7c59ed37a2eccd00a669b761ae6ee60
Micah Wood <micahswood7@gmail.com>
2021-12-08 12:00:44 -0600

    added helo asteroids

---
+++ js/hello-asteroid.js
@@ -0,0 +1,16 @@
+console.log("hello sweet asteroid!");
+
+// console log the result of a string expression
+// which describes one of your favorite foods to eat
+// use the + operator to concatenate the separate strings
+
+console.log("My favorite food is the Spicy chicken sandwhich from Whataburger" + " but replace the mayo with Buffalo sauce and" + "add jalapenos to make it spicier");
+
+// Place the favorite food phrases into variables
+// concatenate those variables inside a console log!
+
+var FavoriteFood = "Spicy chicken Sandwhich from Whataburger"
+
+var Phase1 = "my favorite food is "
+
+console.log(Phase1 + FavoriteFood)
