fadfb9a5d35a04a353a9bbaefbabba668ade63de
Micah Wood <micahswood7@gmail.com>
2021-12-09 16:41:34 -0600

    added JavaScript Practice' cd

--- js/working-with.js
+++ js/working-with.js
@@ -13,3 +13,9 @@ console.log(d++);
 let e = false;
 console.log(e++);
 
+let price = 2.7
+console.log(price.toFixed(2));
+
+console.log(isNaN(0));
+console.log(isNaN(1));
+console.log(isNaN("string"));
