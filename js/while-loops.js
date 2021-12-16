c7138a7bdff95c63aff5e5cff58e50703163f14b
Micah Wood <micahswood7@gmail.com>
2021-12-16 15:38:19 -0600

    practicing Java Script

---
+++ js/while-loops.js
@@ -0,0 +1,12 @@
+"use strict";
+// // TODO: MINI EXERCISE
+// // TODO: print all even numbers between 0 and 25;
+// let counter = 0; // starting point
+
+let counter = 0;
+
+while (counter <= 25) {
+    counter += 2
+    console.log("Your number is: " + counter)
+    console.log("***************************")
+}
