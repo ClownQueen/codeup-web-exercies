179f3e4ab84431f0dfac6bb54b3e69480fe65e95
Micah Wood <micahswood7@gmail.com>
2022-01-20 13:42:14 -0600

    edited morning-warmups.js

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -33,26 +33,27 @@
 //
 // console.log(removeAll(bugs, 'ant'))
 
-let myCats = [
-    {
-        catName: "Fifi",
-        age: 5
-    },
-    {
-        catName: "Fluffy",
-        age: 3
-    },
-    {
-        catName: "Abby",
-        age: 7
-    }
-];
 /** Write a JS function called getNthCat that takes an array of cat objects as a first parameter called cats, and n as a second parameter.
  The function returns the cat object at index n in the cats array. */
 
-function getNthCat(cats, n) {
-    return cats[n]
-}
+// let myCats = [
+//     {
+//         catName: "Fifi",
+//         age: 5
+//     },
+//     {
+//         catName: "Fluffy",
+//         age: 3
+//     },
+//     {
+//         catName: "Abby",
+//         age: 7
+//     }
+// ];
 
-// print out the cat at index 1
-console.log(getNthCat(myCats, 1));
+// function getNthCat(cats, n) {
+//     return cats[n]
+// }
+//
+// // print out the cat at index 1
+// console.log(getNthCat(myCats, 1));
