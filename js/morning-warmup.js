9233a282fc55ce6c0467e576a65f770b9acd2a3e
Micah Wood <micahswood7@gmail.com>
2022-02-01 10:21:48 -0600

    added to morning-warmup.js

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -100,3 +100,28 @@
 // }
 //
 // console.log(sumOfSquares(2,2))
+
+/** Write a function called whichStringIsLonger() that takes two parameters and returns one of 4 values:
+ returns "first" if the first string parameter is longer
+ returns "second" if the second parameter is longer
+ returns "neither" if they are the same length
+ returns false if either or both of the parameters are not strings */
+
+// function whichStringIsLonger(a,b){
+//     while (typeof a === "string" && typeof b === "string"){
+//         if (a.length >  b.length){
+//             return "first is longer"
+//         } else if (a.length < b.length){
+//             return "second is longer"
+//         } else {
+//             return "neither, they are the same length"
+//         }
+//     }
+//     return false
+// }
+// console.log(whichStringIsLonger("bobby", "lou")); // returns first
+// console.log(whichStringIsLonger("bob", "louise")); // returns second
+// console.log(whichStringIsLonger("bob", "lou")); // returns neither
+// console.log(whichStringIsLonger("bobby", 2)); // returns false
+// console.log(whichStringIsLonger(1, "lou")); // returns false
+
